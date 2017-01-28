40525d2f132cda0d4874dcda49402e637ddd4ff9
MANI Adel <mani.adel@yahoo.fr>
2017-01-29 00:57:30 +0100

    tracking part ended

--- assets/js/controllers/providersCrtl.js
+++ assets/js/controllers/providersCrtl.js
@@ -5,6 +5,13 @@ angular.module('justintime').controller('providersCtrl', ['$scope','$http',
 
     	launchMap();
 
+    		
+    		var data ;
+    		$.getJSON("./assets/conf/itiTraces.json", function(json) {
+			    data = json; 
+			});	
+
+
     	$http.get("https://psa.opendatasoft.com/api/records/1.0/search/?dataset=carto_fournisseurs_site&rows=10&apikey=6d9b68a8b2bcc12967438ce7c922eeda33676c778766adfac9468f16")
 		    .then(function(response) {
 		    	var poiList = response.data.records; 
@@ -12,6 +19,28 @@ angular.module('justintime').controller('providersCtrl', ['$scope','$http',
 
 		        console.log(response.data);     
 		 });
+
+		 $scope.doClick = function(item, event){	
+		 	//console.log(" - Clicked Element: ",item.fields.cofor_fnr_liv); 
+		 	console.log( "Data: ", data)
+
+		 	$scope.selectedItem = item; 		 	
+		 	$scope.currentDeliveryList = data;  
+
+		 	$( "#providersList" ).hide( "slow", function() {
+    			
+    			//1. display clicked provider 
+    			//2. Display current delevry List
+    			startMapTracking(data);
+    			
+
+  			});
+
+		 }
+
+
+
+
 
     }
 ]);
--- assets/js/map/map.js
+++ assets/js/map/map.js
@@ -1,24 +1,89 @@
 
 
-
+var myMap = null;
+var marker;
+var markers = [];
 
 function launchMap() {
 	var conf = {
-	  container : $_id("providersMap")
+	  container : $_id("providersMap"),
+	  mapTypeControlOptions : {type: ViaMichelin.Api.Constants.Map.TYPE.LIGHT}
 	};
 
-	VMLaunch("ViaMichelin.Api.Map", conf);
+
+	var callbacks = {
+		onInit: function(serviceMap){
+			myMap = serviceMap;
+		},
+		onClick: function(event){
+	
+		},
+		//If center is a locID or a (dbID, poiID) pair, you must uncomment this section
+		//to set the zoom level defined in the zoom parameter
+		onSuccess: function(){
+			this.refresh();						
+			
+		},
+		onInitError: function(){
+			alert('Whoops Map cannot be loaded!');
+		}
+	}
+
+
+	VMLaunch("ViaMichelin.Api.Map", conf, callbacks);
 }
 
 
-$(".poiProviders").click(function(){
+var position0 =280;
+var position1 =200;
+var position2 =0;
 
-	alert('Yupiiii ');
-  // Holds the product ID of the clicked element
-  /*var productId = $(this).attr('class').replace('addproduct ', '');
-  addToCart(productId); */
+function startMapTracking(data){
+	console.log("start Tracking ...");
+	console.log(data[0].itiTrace[0][1000])
+
+	//init markers
+	markers = []; 
+	myMap.removeAllLayers();
+
+	addMarkers({lon: data[0].itiTrace[0][position0], lat: data[0].itiTrace[1][position0] }, './assets/img/truck.png', "shadown", false, " HI   ",  " HI  REF "+data[0].id, "true");
+	addMarkers({lon: data[0].itiTrace[0][position1], lat: data[0].itiTrace[1][position1] }, './assets/img/truck.png', "shadown", false, " HI   ",  " HI  REF "+data[1].id, "true");
+	addMarkers({lon: data[0].itiTrace[0][position2], lat: data[0].itiTrace[1][position2] }, './assets/img/truck.png', "shadown", false, " HI  ", " HI  REF "+data[2].id, "true");
+	myMap.drawMapFromLayers();
+
+	
+	var moveTimer = setInterval(function moveTonNextCordinates(){
+		console.log("GPS Coords P1: "+position0+"  - P2: "+position1+ " - P3: "+position2);
+
+		markers[0].setPosition({lon: data[0].itiTrace[0][position0], lat: data[0].itiTrace[1][position0] });
+		markers[1].setPosition({lon: data[0].itiTrace[0][position1], lat: data[0].itiTrace[1][position1] });
+		markers[2].setPosition({lon: data[0].itiTrace[0][position1], lat: data[0].itiTrace[1][position2] });
+
+		position0++;
+		position1++;
+		position2++;
+
+	}, 1000);
 
 
 
-});
+}
+
+
+
+
+function addMarkers(coords, icon, shadown, autoOpen, mytitle, infoBubleContent, visibility){
+		//create a marker to display ViaMichelin headquarters
+		   var marker = new ViaMichelin.Api.Map.Marker({
+			coords :  coords,
+			htm : infoBubleContent,
+			//image is 52*36 so offset is half those dimensions
+			icon : {url: icon , offset: {x : -25, y: -25}},
+			autoOpen: "true", 
+			title: mytitle
+			});
+		myMap.addLayer(marker);
+		markers.push(marker)
+	}
+
 
