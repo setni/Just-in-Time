324a0cc413db002921fa555df8a075b2a5dcd229
setni <dupont.thomas70@gmail.com>
2017-01-29 14:22:15 +0100

    style dashboard

--- assets/js/controllers/dashboardCtrl.js
+++ assets/js/controllers/dashboardCtrl.js
@@ -1,5 +1,8 @@
 angular.module('justintime').controller('dashboardCtrl', ['$scope', 'mainFactory',
     function($scope, mainFactory){
-
+        setTimeout(function () {
+            $scope.providersList = $scope.$parent.providersList;
+            console.log($scope.$parent.providersList);
+        }, 1000);
     }
 ]);
